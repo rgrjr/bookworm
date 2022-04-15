8c54d15e7f397782e9b8e928939d10a48b710b0c
Bob Rogers <rogers@rgrjr.com>
2022-04-14 23:58:33 -0400

    Add some "moving" recipes
    * database/db-recipes.sql:
       + Update all book boxes so that they have suitable destinations.
       + Summarize box weights by destination.
       + Move all locations from basement to pod #1.

--- database/db-recipes.sql
+++ database/db-recipes.sql
@@ -21,3 +21,30 @@ delete from book_author_map
     where book_id = 1438;
 delete from book
     where book_id = 1438;
+
+-- Update all book boxes so that they have suitable destinations.
+update location
+    set destination = 'Bob''s office'
+    where name like '%book box%';
+update location
+    set destination = 'Bob''s office'
+    where parent_location_id = 92;
+update location
+    set destination = 'garage'
+    where parent_location_id = 92 and name like 'henge%';
+update location
+    set parent_location_id = 35
+    where parent_location_id = 92;
+
+-- Summarize box weights by destination.
+-- The home page does this, but not using "group by".
+select destination, sum(weight) as total_weight, count(1) as boxes
+    from location
+    where weight > 0
+    group by destination;
+
+-- Move all locations from basement to pod #1.
+update location as loc, location as p1, location as p2
+    set loc.parent_location_id = p2.location_id
+    where p1.name = 'basement' and p2.name = 'Pod #1'
+	  and loc.parent_location_id = p1.location_id;
